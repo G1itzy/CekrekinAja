4b5929a4bb78f20bdc787bcca30d38d72f4c76a9
G1itzy <164884676+G1itzy@users.noreply.github.com>
2025-07-08 23:16:42 +0700

    Update admin_management.blade.php

--- resources/views/admin/user/admin_management.blade.php
+++ resources/views/admin/user/admin_management.blade.php
@@ -1,17 +1,72 @@
 @extends('admin.main')
+
 @section('content')
+<style>
+    /* Tombol dengan ukuran konsisten */
+    .btn-action {
+        width: 100%;
+        padding: 10px;
+        text-align: center;
+        font-size: 14px;
+        border-radius: 5px;
+        margin: 5px 0;
+    }
+
+    /* Tombol untuk Superadmin */
+    .btn-superadmin {
+        background-color: #0d6efd;
+        border-color: #0d6efd;
+    }
+
+    .btn-superadmin:hover {
+        background-color: #0056b3;
+        border-color: #0056b3;
+    }
+
+    /* Tombol untuk Admin */
+    .btn-admin {
+        background-color: #28a745;
+        border-color: #28a745;
+    }
+
+    .btn-admin:hover {
+        background-color: #218838;
+        border-color: #218838;
+    }
+
+    /* Tombol untuk menghapus Admin */
+    .btn-danger {
+        background-color: #dc3545;
+        border-color: #dc3545;
+    }
+
+    .btn-danger:hover {
+        background-color: #c82333;
+        border-color: #c82333;
+    }
+
+    /* Tombol dengan beberapa tombol dalam satu baris (Button group) */
+    .btn-group {
+        display: flex;
+        gap: 10px;
+    }
+</style>
+
 <div class="container-fluid px-4">
+    <!-- Button to add new user -->
     <div class="row">
         <div class="col-12">
             <button type="button" class="btn btn-success mt-4" data-bs-toggle="modal" data-bs-target="#addNewUser">Tambah User</button>
         </div>
     </div>
-    <div class="row">
-        <div class="col col-lg-6 col-md-12">
-            <div class="card shadow mt-4">
+
+    <!-- Users Table -->
+    <div class="row mt-4">
+        <div class="col-lg-6 col-md-12">
+            <div class="card shadow">
                 <div class="card-header"><b>User</b></div>
                 <div class="card-body">
-                    <table id="dataTable">
+                    <table id="dataTable" class="table">
                         <thead>
                             <tr>
                                 <th>No.</th>
@@ -29,11 +84,22 @@
                                 <td>{{ $item->email }}</td>
                                 <td>{{ $item->telepon }}</td>
                                 <td>
-                                    <form action="{{ route('user.promote', ['id' => $item->id]) }}" method="POST">
-                                        @method('PATCH')
-                                        @csrf
-                                        <button type="submit" class="btn btn-success">Jadikan Admin</a>
-                                    </form>
+                                    @if (Auth::user()->role == 2) <!-- Only Superadmin can see these buttons -->
+                                        <div class="btn-group w-100">
+                                            <!-- Promote to Admin -->
+                                            <form action="{{ route('user.promote', ['id' => $item->id]) }}" method="POST" class="d-inline">
+                                                @csrf
+                                                @method('PATCH')
+                                                <button type="submit" class="btn btn-admin btn-sm btn-action">Jadikan Admin</button>
+                                            </form>
+                                            <!-- Promote to Superadmin -->
+                                            <form action="{{ route('user.promoteToSuperAdmin', ['id' => $item->id]) }}" method="POST" class="d-inline">
+                                                @csrf
+                                                @method('PATCH')
+                                                <button type="submit" class="btn btn-superadmin btn-sm btn-action">Jadikan Superadmin</button>
+                                            </form>
+                                        </div>
+                                    @endif
                                 </td>
                             </tr>
                             @endforeach
@@ -42,8 +108,10 @@
                 </div>
             </div>
         </div>
-        <div class="col col-lg-6 col-md-12">
-            <div class="card shadow mt-4">
+
+        <!-- Admin Table -->
+        <div class="col-lg-6 col-md-12">
+            <div class="card shadow">
                 <div class="card-header"><b>Admin</b></div>
                 <div class="card-body">
                     <table class="table">
@@ -63,10 +131,11 @@
                                 <td>{{ $item->telepon }}</td>
                                 <td>
                                     @if ($item->id != Auth::user()->id)
-                                        <form action="{{ route('user.demote',['id' => $item->id]) }}" method="POST">
-                                            @method('PATCH')
+                                        <!-- Demote Admin -->
+                                        <form action="{{ route('user.demote', ['id' => $item->id]) }}" method="POST" class="d-inline">
                                             @csrf
-                                            <button type="submit" class="btn btn-danger">Cabut Admin</button>
+                                            @method('PATCH')
+                                            <button type="submit" class="btn btn-danger btn-sm btn-action">Cabut Admin</button>
                                         </form>
                                     @endif
                                 </td>
@@ -80,6 +149,7 @@
     </div>
 </div>
 
+<!-- Modal to Add New User -->
 <div class="modal fade" id="addNewUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -112,4 +182,5 @@
         </div>
     </div>
 </div>
+
 @endsection
